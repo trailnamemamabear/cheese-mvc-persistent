d6eff8751f1ac3c17ceed15d23be4d074c89b6ce
Holly Parker <hollyfparker@gmail.com>
2019-09-10 11:43:45 -0500

    commit

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,26 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("category")
+
+public class CategoryController {
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model){
+
+    }
+}
